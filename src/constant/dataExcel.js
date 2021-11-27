97c3573f33c22459dfe4a28860ce39ee82305916
anhtt2211 <trananh22112001@gmail.com>
2021-11-28 02:11:31 +0700

    add data constant to export excel

--- src/constant/dataExcel.js
+++ src/constant/dataExcel.js
@@ -6,7 +6,13 @@ import {
   lecturerState$,
   studentState$,
 } from 'redux/selectors';
-import { mapToEmployee } from 'utils/dataExcel';
+import {
+  mapToClass,
+  mapToCourse,
+  mapToEmployee,
+  mapToLecturer,
+  mapToStudent,
+} from 'utils/dataExcel';
 
 const employees = useSelector(employeeState$);
 const lecturers = useSelector(lecturerState$);
@@ -15,5 +21,9 @@ const courses = useSelector(courseState$);
 const classes = useSelector(classState$);
 
 const employeesData = mapToEmployee(employees.data);
+const lecturersData = mapToLecturer(lecturers.data);
+const studentsData = mapToStudent(students.data);
+const coursesData = mapToCourse(courses.data);
+const classesData = mapToClass(classes.data);
 
-export { employeesData };
+export { employeesData, lecturersData, studentsData, coursesData, classesData };

68ef5d8cbcf4eb38fb791425087c601c04cb0d05
anhtt2211 <trananh22112001@gmail.com>
2021-12-01 21:58:22 +0700

    fix missing file saga after merge from dev

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -5,10 +5,13 @@ import { courseTypeSaga } from 'redux/sagas/courseTypes';
 import { levelSaga } from 'redux/sagas/levels';
 import { parameterSaga } from 'redux/sagas/parameters';
 import { authSaga } from './auth';
+import { billSaga } from './bills';
 import { classSaga } from './classes';
 import { employeeSaga } from './employees';
+import { examSaga } from './exams';
 import { lecturerSaga } from './lecturers';
 import { studentSaga } from './students';
+import { testTypeSaga } from './testTypes';
 import { timeFrameSaga } from './timeFrames';
 import { userSaga } from './users';
 
@@ -27,5 +30,8 @@ export default function* mySaga() {
     authSaga(),
     timeFrameSaga(),
     classSaga(),
+    billSaga(),
+    examSaga(),
+    testTypeSaga(),
   ]);
 }
